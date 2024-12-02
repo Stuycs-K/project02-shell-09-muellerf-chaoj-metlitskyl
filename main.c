586afaa75168c55d90f6e271db017a2175817e35
Jason Chao <jchao50@stuy.edu>
2024-12-02 14:51:22 -0500

    Added echo functionality

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(int argc, char **argv) {
+    printf("Entry to update: ");
+    fgets(buffer, 255, stdin);
+    sscanf(buffer, "%d", &line);
     return 0;
 }
