5666d0ef80f7cb545e398468c35813eb4a449fe2
Jason Chao <jchao50@stuy.edu>
2024-12-03 14:26:07 -0500

    Implemented echo

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-    printf("Entry to update: ");
-    fgets(buffer, 255, stdin);
-    sscanf(buffer, "%d", &line);
+    char buffer[256];
+    while (fgets(buffer, 255, stdin) != NULL) {
+        buffer[strcspn(buffer, "\r\n")] = 0;
+        if (strcmp(buffer, "exit") == 0) {
+            exit(0);
+        }
+        printf("your string: %s\n\n", buffer);
+    }
     return 0;
 }
