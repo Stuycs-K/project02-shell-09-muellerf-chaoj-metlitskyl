c17fd551c7cb877741ab5cdae524dfb87a2f5b9f
Jason Chao <jchao50@stuy.edu>
2024-12-03 14:34:08 -0500

    Implemented execvp

--- main.c
+++ main.c
@@ -5,12 +5,15 @@
 
 int main(int argc, char **argv) {
     char buffer[256];
+    long arg_max = sysconf(_SC_ARG_MAX);
     while (fgets(buffer, 255, stdin) != NULL) {
         buffer[strcspn(buffer, "\r\n")] = 0;
         if (strcmp(buffer, "exit") == 0) {
             exit(0);
         }
-        printf("your string: %s\n\n", buffer);
+        char ** cmdargv = calloc(arg_max, sizeof(char *));
+        parse_args(buffer, cmdargv);
+        execvp(cmdargv[0], cmdargv);
     }
     return 0;
 }
