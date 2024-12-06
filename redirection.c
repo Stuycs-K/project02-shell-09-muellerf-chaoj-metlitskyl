11b5a47606a7eade25b7f7e5a32a09eee09f346c
jsonano <jchao5768@gmail.com>
2024-12-05 22:29:06 -0500

    Implemented stdout redirection functions

--- redirection.c
+++ redirection.c
@@ -4,18 +4,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 void stdout_redirect(char * output) {
-    int output_file = open(output, 0_WRONLY | O_TRUNC | O_CREAT);
+    int output_file = open(output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     int stdout = STDOUT_FILENO;
+    int backup = dup(stdout);
     dup2(output_file, stdout);
     printf("File successfully redirected.");
+    close(output_file);
+    fflush(stdout); // not necessary in child processes
+    dup2(backup, stdout);
 }
 
 void stdout_redirect_append(char * input, char * output) {
-    
+    int output_file = open(output, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    int stdout = STDOUT_FILENO;
+    int backup = dup(stdout);
+    dup2(output_file, stdout);
+    printf("File successfully redirected.");
+    close(output_file);
+    fflush(stdout);
+    dup2(backup, stdout);
 }
 
-void stdin_redirect() {
-    
+void stdin_redirect(char * input) {
+    // may not be necessary as you can just use the new file as stdin once '<' is detected
 }
--- redirection.h
+++ redirection.h
@@ -2,5 +2,5 @@
 #define REDIRECTION_H
 void stdout_redirect(char * output);
 void stdout_redirect_append(char * input, char * output);
-void stdin_redirect();
+void stdin_redirect(char * input);
 #endif
