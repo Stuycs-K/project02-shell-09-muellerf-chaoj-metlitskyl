8d1cd0c308e5d786b0b25ca4d6e791f70c26321e
leomet07 <leomet07@gmail.com>
2024-12-07 17:18:22 -0500

    fix bug of child living on even if command does not exist

--- parse.c
+++ parse.c
@@ -7,7 +7,11 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void handleChild(char **cmdargv) { execvp(cmdargv[0], cmdargv); }
+void handleChild(char **cmdargv) {
+    execvp(cmdargv[0], cmdargv);
+    printf("%s\n", strerror(errno));
+    exit(errno); // this is only executed if execvp fails (so child does not keep on living)
+}
 
 void handlePossibleForkFail(pid_t p) {
     if (p < 0) {
@@ -39,9 +43,8 @@ void handle_line_input(char *buffer) {
     }
     char **cmdargv = calloc(arg_max, sizeof(char *));
     parse_args(buffer, cmdargv);
-    if (strcmp(cmdargv[0], "") == 0) {
-        return; // don't do anything (execing nothing just forks unncessarily)
-        // TODO: This check fails if we have characters like up arrow key or somehting
+    if (strcmp(cmdargv[0], "") == 0 || cmdargv[0][0] == 27) {
+        return; // don't do anything if blank or escape char (execing nothing just forks unncessarily)
     }
 
     pid_t child_one;
