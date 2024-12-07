43b258a963b76608dcf6942dff06afc0160f3402
leomet07 <leomet07@gmail.com>
2024-12-07 16:59:03 -0500

    Replace home prefix with tilde

--- utils.c
+++ utils.c
@@ -6,6 +6,10 @@
 #include <string.h>
 #include <unistd.h>
 
+int prefix(const char *pre, const char *str) {
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
+
 void print_prompt() {
     char username[LOGIN_NAME_MAX + 1];
     getlogin_r(username, sizeof(username)); // check the return value!
@@ -13,19 +17,24 @@ void print_prompt() {
     char hostname[HOST_NAME_MAX + 1];
     gethostname(hostname, sizeof(hostname)); // check the return value!
 
-    char cwd[PATH_MAX];
+    char *cwd = calloc(PATH_MAX, sizeof(char));
+    char *cwd_formatted = cwd;
     getcwd(cwd, PATH_MAX); // check the return value!
-
-    char prompt[LOGIN_NAME_MAX + HOST_NAME_MAX + PATH_MAX + 512] = "";
-    sprintf(prompt, "%s@%s %s$ ", username, hostname, cwd);
-
-    // replace "/home/user" from cwd
+                           // replace "/home/user" from cwd
     char home_prefix[] = "/home/";
     size_t path_target_len = strlen(home_prefix) + strlen(username) + 1;
     char *path_target = calloc(path_target_len, sizeof(char));
     strcat(path_target, home_prefix);
     strcat(path_target, username);
+    if (prefix(path_target, cwd)) {
+        cwd_formatted[path_target_len - 2] = '~';
+        cwd_formatted = cwd + path_target_len - 2; // subtract 2 instead of 1 to account for '~'
+    }
+
+    char prompt[LOGIN_NAME_MAX + HOST_NAME_MAX + PATH_MAX + 512] = "";
+    sprintf(prompt, "%s@%s %s$ ", username, hostname, cwd_formatted);
 
     printf(prompt);
     fflush(stdout);
+    free(cwd);
 }
